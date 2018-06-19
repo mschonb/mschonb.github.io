72fab57816bf02a13a6cb2d2821a1d11bc2b814b
mschonb <mschonb@gmail.com>
2018-06-19 11:01:01 -0700

    Top image change, new section: Mis/Vis

--- js/agency.js
+++ js/agency.js
@@ -1,7 +1,6 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
-  // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -47,4 +46,4 @@
     $(".navbar").removeClass("d-none");
   })
 
-})(jQuery); // End of use strict
+})(jQuery);
